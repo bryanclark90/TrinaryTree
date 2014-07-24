54315124a3fce840a39d4fed92287549761b081e
Bryan Clark <bryan.clark@email.wsu.edu>
2014-07-24 11:47:50 -0700

    Create TriTree.h
    Header file for Trinary Tree. Includes the TriTree class which has nodes for left,right,and middle. as well as the data values. Contains root node, and all functions necessary to construct a Trinary Tree

---
+++ TriTree.h
@@ -0,0 +1,50 @@
+//Programmer: Bryan K Clark
+//Email: bryan.clark@email.wsu.edu
+//Phone: (509)294-4161
+
+#include <iostream>
+#include <queue>
+#include <stdio.h>
+#include <tchar.h>
+
+
+//create a Trinary Tree class
+class TriTree
+{
+private:
+	struct Node
+	{
+		int Data;
+		Node* Left;
+		Node* Middle;
+		Node* Right;
+
+		Node(int dataValue);
+	};
+
+	//pointer to root node
+	//if NULL the tree is empty
+	Node* m_root;
+
+	void DeleteTree(Node* n);
+	void DisplayContents(Node* node, std::ostream& outputStream);
+	// Adds in a new node
+	bool insertNode(Node* TempRoot, Node* NewNode );
+public:
+	//constructor and deconstructor
+	TriTree(void);
+	~TriTree(void);
+
+
+	//returns true if successfully added
+	bool Add(int dataValue);
+	//returns true if  the value was found and removed, else false
+	bool Remove(int dataValue);
+	//returns true if the node is successfully removed
+	bool removeNode(Node* parent, Node* grandparent, int dataValue);
+	//checks if tree is empty. True if empty
+	bool IsEmpty(void);
+
+	void DisplayContents(std::ostream& outputStream);
+
+};
